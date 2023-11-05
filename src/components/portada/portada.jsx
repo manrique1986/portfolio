0777ecbd45058b0fe390076aad3dd3f660e8eb12
Leandro Martini <leandromartini86@gmail.com>
2023-11-05 16:41:43 -0300

    probando portada

--- src/components/portada/portada.jsx
+++ src/components/portada/portada.jsx
@@ -9,7 +9,7 @@ const portada = () => {
         <img
           className="float-right w-50"
           src="/src/assets/imagenYo.png"
-          alt=""
+          alt="imgPortada"
         />
         <div className="grid lg:grid-cols-2 all:grid-cols-1 mx-28">
           <div className=" mt-36">
