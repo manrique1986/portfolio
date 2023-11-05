321022fddab710a16ea1e5017abf7f7775d5bd51
Leandro Martini <leandromartini86@gmail.com>
2023-11-04 23:41:28 -0300

    portada imagen

--- src/components/portada/portada.jsx
+++ src/components/portada/portada.jsx
@@ -3,14 +3,15 @@ import imgPortada from "../../assets/Diseño sin título (3).png";
 
 const portada = () => {
   return (
-    <div className="bg-img-fondo bg-cover w-screen h-screen ">
+    <div className="bg-[url('/src/assets/fondoPortada.png')]  lg:w-screen lg:h-screen ">
       <div  >
-        <div className="grid grid-cols-2 mx-28">
+        <img className="float-right w-50" src="/src/assets/imagenYo.png" alt="" />
+        <div className="grid lg:grid-cols-2 all:grid-cols-1 mx-28">
           <div className="mt-36">
-            <p className="text-[#EC6625] font-bold text-[74px]">
+            <p className="text-[#EC6625] lg:ml-20 font-bold text-[55px]">
               Leandro Martini
             </p>
-            <h1 className=" mt-8 ml-10 npm run detext-[65px]">
+            <h1 className=" mt-8 ml-10 npm run text-[45px]">
               Programador frontend
             </h1>
           </div>
