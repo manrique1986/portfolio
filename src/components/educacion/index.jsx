d1cfc8684e8c752b7ac01d17b179191beb000e81
Leandro Martini <leandromartini86@gmail.com>
2023-11-23 19:55:15 -0300

    arreglos en educacion

--- src/components/educacion/index.jsx
+++ src/components/educacion/index.jsx
@@ -13,7 +13,50 @@ const index = () => {
                 Educacion
               </h1>
             </div>
-            <section className=" lg:ml-96 all:mt-28 justify-center all:px-11 ">
+            <div className="lg:ml-96 all:mt-28 justify-center all:px-11">
+              {/* Coderhouse */}
+              <div className="mb-6">
+                <h3 className="text-[#EB3A00] all:text-2xl all:mt-8 lg:mt-16 font-semibold lg:text-[35px]">
+                  Coderhouse (Junio 2021 - Marzo 2022)
+                </h3>
+                <p className="font-extrabold mt-4">
+                  Carrera de Desarrollador Frontend:
+                </p>
+                <ul className="list-inside list-disc pl-6 mb-2 font-bold pt-4">
+                  <li>Desarrollo Web</li>
+                  <li>Javascript</li>
+                  <li>React</li>
+                  <li>Curso adicional (Marzo 2022): Vue.js</li>
+                </ul>
+              </div>
+
+              {/* Educacion IT */}
+              <div className="mb-6">
+                <h3 className="text-[#EB3A00] all:text-2xl all:mt-8 lg:mt-16 font-semibold lg:text-[35px]">
+                  Educacion IT (Febrero 2023 - Mayo 2023)
+                </h3>
+                <ul className="list-inside list-disc pl-6 mb-2 font-bold pt-4">
+                  <li>Curso de React</li>
+                  <li>Node.js</li>
+                </ul>
+              </div>
+
+              {/* Udemy */}
+              <div>
+                <h3 className="text-[#EB3A00] all:text-2xl all:mt-8 lg:mt-16 font-semibold lg:text-[35px]">
+                  Udemy (Agosto 2023 - Actualidad)
+                </h3>
+                <ul className="list-inside list-disc pl-6 mb-2 font-bold pt-4">
+                  <li>Curso actual (Actualidad): Node.js</li>
+                  <li>
+                    {" "}
+                    Estudios en curso (Actualidad): Buenas Prácticas en
+                    Javascript
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* <section className=" lg:ml-96 all:mt-28 justify-center all:px-11 ">
               <div>
                 <h1 className=" text-[#EB3A00] all:text-2xl all:mt-8 lg:mt-16  lg:text-[35px]">
                   Coderhouse Junio de 2021 - Marzo de 2022
@@ -22,24 +65,14 @@ const index = () => {
                
                 </h3>
                 <h3 className="font-bold ">     En el mes de Marzo del 2022 realice el curso de Vue</h3>
-                {/* <ul className="lg:pt-6 ">
-                  <li>Carrera Desarrollo Frontend React <span className="text-orange-700">Junio de 2021 a Noviembre 2021</span></li>
-                  <li className="ml-4">Desarrollo web</li>
-                  <li className="ml-4">Javascript</li>
-                  <li className="ml-4">React</li>
-                  <li>Vue <span className="text-orange-700">Marzo de 2022</span></li>
-                </ul> */}
+             
               </div>
               <div className="">
                 <h1 className=" text-[#EB3A00] all:text-2xl all:mt-8 lg:mt-16  lg:text-[35px]">
                  Educacion it Febrero 2023 - Mayo 2023
                 </h1>
                 <h3 className="font-bold pt-4 "> Realice el curso de React y posteriormente comencé a explorar Node.js.</h3>
-                {/* <ul className="lg:pt-6">
-                  <li>React <span className="text-orange-700">Febrero de 2023</span></li>
-                
-                  <li>Node js <span className="text-orange-700">Mayo 2023</span></li>
-                </ul> */}
+              
               </div>
               <div className="">
                 <h1 className=" text-[#EB3A00] all:text-2xl all:mt-8 lg:mt-16 lg:text-[35px]">
@@ -49,7 +82,7 @@ const index = () => {
                 Me encuentro realizando curso de Node JS y realizando estudios de buenas practicas en Javascript
                </h3>
               </div>
-            </section>
+            </section> */}
           </div>
         </div>
       </Layout>
