e12da9c69815babf72580f943db8e8f0c6ce7bb6
Leandro Martini <leandromartini86@gmail.com>
2023-11-23 20:30:06 -0300

    agregando component contacto

--- src/components/contacto/contacto.jsx
+++ src/components/contacto/contacto.jsx
@@ -1,21 +1,95 @@
 import React from "react";
 import Layout from "../layout";
+import { FaEnvelope, FaPhone } from "react-icons/fa";
 
-const contacto = () => {
+const Contact = () => {
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para enviar el formulario, como usar un servicio de envío de correos.
+    // Por ahora, simplemente mostraremos un mensaje de éxito.
+    alert("¡Correo enviado con éxito!");
+  };
+
   return (
     <Layout>
-      <div className="flex">
-        <div className="bg-img-contacto bg-cover w-screen h-screen  bg-top">
-          <div className="   lg:ml-64 all:mt-2    lg:mt-14 bg-[#F7B200]   lg:h-[60px] lg:w-[275.8px] lg:pt-2  rounded-xl all:mx-11 ">
-            <h1 className="justify-center items-center all:text-[30px]    font-bold text-center text-[#EB3A00]">
-              Contacto
-            </h1>
+      <div className="bg-img-proyectos bg-cover w-screen h-screen  bg-top">
+        <div className="min-h-screen flex items-center justify-center ">
+         
+          <div className="lg:w-5/12 all:w-4/5  lg:p-8 all:p-2 rounded-md shadow-md">
+          <h3 className="text-[#EB3A00] font-semibold lg:text-[35px] mb-6">Leandro Martini</h3>
+           
+
+            <div className="flex items-center mb-6">
+              <FaEnvelope className="text-2xl mr-4" />
+              <p>leandromartini86@gmail.com</p>
+            </div>
+
+            <div className="flex items-center mb-6">
+              <FaPhone className="text-2xl mr-4" />
+              <p>(11) 3037-4277</p>
+            </div>
+
+            <form onSubmit={handleFormSubmit}>
+              <div className="mb-4">
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-semibold text-gray-600"
+                >
+                  Nombre:
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="w-full p-2 border rounded-md"
+                  required
+                />
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-semibold text-gray-600"
+                >
+                  Correo electrónico:
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full p-2 border rounded-md"
+                  required
+                />
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-semibold text-gray-600"
+                >
+                  Mensaje:
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  rows="4"
+                  className="w-full p-2 border rounded-md"
+                  required
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="w-full bg-[#EB3A00] text-white px-4 py-2 rounded-md hover:bg-[#F7B200]"
+              >
+                Enviar
+              </button>
+            </form>
           </div>
-          <div></div>
         </div>
       </div>
     </Layout>
   );
 };
 
-export default contacto;
+export default Contact;
